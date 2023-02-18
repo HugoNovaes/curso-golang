f9f593624bf702e56fd16ed2a2ef12cc8a031fed
Hugo Novaes <hugo.novaes@crk.com.br>
2023-02-18 11:31:16 -0300

    aula1

---
+++ commit.bat
@@ -0,0 +1,7 @@
+echo "# curso-golang" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/HugoNovaes/curso-golang.git
+git push -u origin main
